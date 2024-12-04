0faf4720fd6ffbdc63727049b499c1b2214b4e67
mahmud <bappimatubber1997@gmail.com>
2024-12-05 02:28:11 +0600

    Second commit for super variable, constant-define and const,single line comment and multiline comment

--- myprog.php
+++ myprog.php
@@ -19,8 +19,37 @@ $name = "Mahmud Islam";
 
 function displayGlobalName(){
 //we need to mention global before a variable that would be used in the function
+   //we can't use const in the function level it is only usable in the class level
+   //so here we can't write "const PI = 3.14" it throws an error every time.
+   //so most of the time we should use define instead of using of const, because we can
+   //use define everywhere and access it from everywhere
+   //const PI = 3.14;
    global $name;
    echo "\nMy name is $name";
 }
 
 displayGlobalName();
+
+//super variable
+//$_GET, $_POST, $_SERVER, $_REQUEST $_SESSION
+
+//CONSTANTS
+//Here SITE_NAME act as a variable and the Shopper act as a variable value
+define("SITE_NAME","Shopper");
+echo SITE_NAME;
+
+//class level const
+class Test{
+   const PI = 3.14;
+
+}
+
+//here :: called as "scope resolution" operator
+echo "\nValue of PI is: ".Test::PI;
+
+
+//single line comment keyboard: ctrl + /
+
+/* for multiline comment
+ we can use 
+shift + Alt + A */
